9e56dbb839784c564c8a9275d39b224b65f3932d
Steven Xiao <xiaoxi328@gmail.com>
2018-05-10 11:58:15 +0000

    init repo

---
+++ include/tlpi_hdr.h
@@ -0,0 +1,28 @@
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include "get_num.h"
+#include "error_functions.h"
+
+#ifdef TRUE
+#undef TRUE
+#endif
+
+#ifdef FALSE
+#undef FALSE
+#endif
+
+typedef enum {FALSE, TRUE} Boolean;
+
+#define min(m,n) ((m) < (n) ? (m) : (n))
+#define max(m,n) ((m) > (n) ? (m) : (n))
+
+#endif 
